82c6f194b5218f9aa72b17cecce2828e3fa63954
Chris Holland <chrisholland3553@gmail.com>
2019-09-21 12:51:47 -0700

    Small changes. Added stdin

--- Assignment-1/shellInterpreter.c
+++ Assignment-1/shellInterpreter.c
@@ -2,13 +2,28 @@
 #include <stdlib.h>
 #include <sys/unistd.h>
 
-int main() {
+#define MAX_INPUT 128
+
+/*
+*   Print the prompt as it changes
+*/
+void printPrompt() {
     char *login;
     login = getlogin();
     char hostname[128];
     gethostname(hostname, sizeof(hostname));
     char cwd[128];
     getcwd(cwd, sizeof(cwd));
-    printf("SSI: %s@%s: %s >\n", login, hostname, cwd);
+    printf("SSI: %s@%s: %s > ", login, hostname, cwd);
+}
+
+int main() {
+    char input[MAX_INPUT];
+
+    for(;;) {
+        printPrompt();
+        fgets(input, MAX_INPUT, stdin);
+    }
+
     return 0;
 }
