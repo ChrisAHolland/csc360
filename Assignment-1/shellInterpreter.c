a52f62fd33f50da00318869e4b2392da1e4d4bd4
Chris Holland <chrisholland3553@gmail.com>
2019-09-30 20:07:04 -0700

    bg has issue with some commands

--- Assignment-1/shellInterpreter.c
+++ Assignment-1/shellInterpreter.c
@@ -11,6 +11,91 @@
 #define MAX_INPUT 128
 
 /*
+*   Linked list node
+*/
+typedef struct Node {
+    char command[256];
+    pid_t pid;
+    struct Node* next;
+} node;
+
+// Create head node for the list
+node* head = NULL;
+
+// Number of nodes in the list
+int listLength = 0;
+
+/*
+*   Adds a new node to the list
+*/
+void add(char** command, pid_t pid) {
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->pid = pid;
+    int i = 0;
+
+    while(command[i] != NULL) {
+        strcat(newNode->command, command[i]);
+        strcat(newNode->command, " ");
+        i++;
+    }
+
+    if (listLength == 0) {
+        head = newNode;
+    } else {
+        node* temp = head;
+
+        while(temp->next != NULL) 
+            temp = temp->next;
+        
+        temp->next = newNode;
+    }
+    newNode->next = NULL;
+    listLength++;
+}
+
+void delete(pid_t pid) {
+    node* temp = head;
+    listLength--;
+    
+    if (head->pid == pid) {
+        printf("%d: %s has terminated.\n", temp->pid, temp->command);
+        head = head->next;
+    } else {
+        while(temp->next->pid != pid) 
+            temp = temp->next;
+
+        printf("%d: %s has terminated.\n", temp->next->pid, temp->next->command);
+        temp->next = temp->next->next;
+        free(temp->next);
+    }
+}
+
+/*
+*   Lists the currently running background commands
+*/
+void listNodes() {
+    node* temp = head;
+
+    while(temp!= NULL) {
+        printf("%d: %s\n", temp->pid, temp->command);
+        temp = temp->next;
+    }
+
+    printf("Total Background Jobs: %d\n", listLength);
+}
+
+void checkProcesses() {
+    if (listLength > 0) {
+        pid_t pid = waitpid(0, NULL, WNOHANG);
+
+        while(pid > 0) {
+            delete(pid);
+            pid = waitpid(0, NULL, WNOHANG);
+        }
+    }
+}
+
+/*
 *   Creates and returns the prompt
 */
 char* printPrompt() {
@@ -45,6 +130,7 @@ void runCommand(char** commands) {
     }
 }
 
+// Handles the cd command
 void changeDirectory(char** path) {
     int ret;
 
@@ -56,6 +142,17 @@ void changeDirectory(char** path) {
     }
 }
 
+void addBackground(char** commands) {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        execvp(commands[1], commands + 1);
+        exit(1);
+    } else if (pid > 0) {
+        add(commands, pid);
+    }
+}
+
 int main() {
     for(;;) {
         // Print the prompt and retrieve user input
@@ -63,6 +160,9 @@ int main() {
         // Tokenize the user input
         char* args = strtok(input, " ");
         char* tokens[256];
+
+        // Check for terminating processes
+        checkProcesses();
 
         int i = 0;
         while (args) {
@@ -74,6 +174,10 @@ int main() {
         // Check if its a "cd" command
         if (!strcmp(tokens[0], "cd")) {
             changeDirectory(tokens);
+        } else if (!strcmp(tokens[0], "bg")) {
+            addBackground(tokens);
+        } else if (!strcmp(tokens[0], "bglist")) {
+            listNodes();
         } else {
             runCommand(tokens);
         }
